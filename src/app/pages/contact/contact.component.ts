e1078e92fb547160b5f83ea5cf7cdfdcca4a5d08
sei <sei.tu@neutec.com.tw>
2026-03-16 10:59:20 +0800

    feat: add ContactComponent public page

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SeoService } from '../../core/seo.service';
+
+const SITE_URL = 'https://idol-genealogy.pages.dev';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './contact.component.html',
+})
+export class ContactComponent implements OnInit {
+  constructor(private seo: SeoService) {}
+
+  ngOnInit() {
+    this.seo.setPage(
+      '聯絡我們 | 台灣地下偶像族譜',
+      '有任何資料錯誤或補充建議，歡迎與我們聯絡。',
+      `${SITE_URL}/contact`
+    );
+  }
+}
