b521540a9b909f6d3417888ca02da2658f394fae
sei <sei.tu@neutec.com.tw>
2026-05-16 16:06:35 +0800

    perf(http): use native fetch for HttpClient to enable SSR HTTP cache

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
-    provideHttpClient(), provideClientHydration()
+    provideHttpClient(withFetch()), provideClientHydration()
   ]
 };
