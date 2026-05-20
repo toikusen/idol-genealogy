8ea7c04224a676da8adbf12b4da56395382c06d3
sei <sei.tu@neutec.com.tw>
2026-05-20 11:25:49 +0800

    feat: update google calendars api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   supabaseAnonKey: 'sb_publishable_PtKb4LIJeJN3cECUJllW7w_UFRVTbTv',
   googleCalendar: {
     calendarId: 'mr7kibfjcm3gu52v6t64lreras@group.calendar.google.com',
-    apiKey: 'AIzaSyCPFsVDUhjZ0HtjNJMI7VNBsSSfxV_XXIQ',
+    apiKey: 'AIzaSyCbDv6bDuzf5ZTpwMxCRkffXhDQoa5s3ss',
   },
 };
