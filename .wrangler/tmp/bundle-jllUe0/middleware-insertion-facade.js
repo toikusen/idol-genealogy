f91c7b82f88ed8d10ac71779e383200fd90b80e1
sei <sei.tu@neutec.com.tw>
2026-05-22 13:18:46 +0800

    feat: fix chunk

---
+++ .wrangler/tmp/bundle-jllUe0/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/Users/seitumbp2025/idol-genealogy/.wrangler/tmp/pages-bXa8DO/functionsWorker-0.6255539179925551.mjs";
+				import * as __MIDDLEWARE_0__ from "/Users/seitumbp2025/idol-genealogy/node_modules/.pnpm/wrangler@4.93.0/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/Users/seitumbp2025/idol-genealogy/node_modules/.pnpm/wrangler@4.93.0/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/Users/seitumbp2025/idol-genealogy/.wrangler/tmp/pages-bXa8DO/functionsWorker-0.6255539179925551.mjs";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
